3350c9de604aad6b46b09068167440f1381b0101
Ben Arnold <ben@arnoldfamily.me.uk>
2021-01-27 09:43:48 +0000

    🎉 Initial project

---
+++ src/SFA.DAS.ApprenticeCommitments.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SFA.DAS.ApprenticeCommitments.Web.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string Name { get; private set; }
+
+        public void OnGet()
+        {
+            Name = "Get called";
+        }
+    }
+}
