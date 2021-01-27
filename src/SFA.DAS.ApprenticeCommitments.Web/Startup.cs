4a06222576f13815353615fea0adc9366ad7a1c5
Ben Arnold <ben@arnoldfamily.me.uk>
2021-01-27 19:58:06 +0000

    🔧 Add Data Protection

--- src/SFA.DAS.ApprenticeCommitments.Web/Startup.cs
+++ src/SFA.DAS.ApprenticeCommitments.Web/Startup.cs
@@ -3,22 +3,28 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SFA.DAS.ApprenticeCommitments.Web.StartupConfiguration;
 
 namespace SFA.DAS.ApprenticeCommitments.Web
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
         {
             Configuration = configuration;
+            Environment = environment;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+
+            services
+                .AddDataProtection(Configuration, Environment);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
