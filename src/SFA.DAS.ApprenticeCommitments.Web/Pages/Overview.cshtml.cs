698d20514854ee3eb70679b322f6bf4725a7d671
Ben Arnold <ben@arnoldfamily.me.uk>
2021-03-04 07:27:41 +0000

    🚧 Authorise the overview page

--- src/SFA.DAS.ApprenticeCommitments.Web/Pages/Overview.cshtml.cs
+++ src/SFA.DAS.ApprenticeCommitments.Web/Pages/Overview.cshtml.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SFA.DAS.ApprenticeCommitments.Web.Pages
 {
+    [Authorize]
     public class OverviewModel : PageModel
     {
         public void OnGet()
