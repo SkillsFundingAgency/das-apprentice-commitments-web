60b210b182839ca58603b541eedd4159af403031
Ben Arnold <ben@arnoldfamily.me.uk>
2021-03-12 11:11:43 +0000

    👌 Remove unnecessary code

--- src/SFA.DAS.ApprenticeCommitments.Web/IdentityHashing/InvalidHashedIdException.cs
+++ src/SFA.DAS.ApprenticeCommitments.Web/IdentityHashing/InvalidHashedIdException.cs
@@ -6,17 +6,8 @@ namespace SFA.DAS.ApprenticeCommitments.Web.Pages.IdentityHashing
 {
     public class InvalidHashedIdException : Exception
     {
-        public InvalidHashedIdException()
-        {
-        }
-
         public InvalidHashedIdException(string? hashValue)
             : base($"Invalid hashed ID value '{hashValue}'")
-        {
-        }
-
-        public InvalidHashedIdException(string message, Exception innerException)
-            : base(message, innerException)
         {
         }
     }
