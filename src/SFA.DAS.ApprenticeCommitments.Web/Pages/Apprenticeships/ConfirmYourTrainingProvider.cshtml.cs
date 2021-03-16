9bbc60e548f242d9f7418e7ff503914fab6ca722
mld3 <57175069+MarieDale3@users.noreply.github.com>
2021-03-16 14:55:17 +0000

    CS-272 small change

--- src/SFA.DAS.ApprenticeCommitments.Web/Pages/Apprenticeships/ConfirmYourTrainingProvider.cshtml.cs
+++ src/SFA.DAS.ApprenticeCommitments.Web/Pages/Apprenticeships/ConfirmYourTrainingProvider.cshtml.cs
@@ -1,14 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using SFA.DAS.ApprenticeCommitments.Web.Services;
 
 namespace SFA.DAS.ApprenticeCommitments.Web.Pages.Apprenticeships
 {
-    public class ConfirmYourTrainingDetailsModel : PageModel, IBacklink
+    public class ConfirmYourTrainingDetailsModel : PageModel
     {
         [BindProperty(SupportsGet = true)]
         public string? ApprenticeshipId { get; set; }
-        public string Backlink => $"/apprenticeships/{ApprenticeshipId}";
 
         public void OnGet()
         {
