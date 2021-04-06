bc89754d2f89ed7fed08db769c75c7ed13872d6a
mld3 <57175069+MarieDale3@users.noreply.github.com>
2021-04-06 11:43:46 +0100

    CS-392 test work

--- src/SAF.DAS.ApprenticeCommitments.Web.UnitTests/Features/HowYourApprenticeshipWillBeDeliveredSteps.cs
+++ src/SAF.DAS.ApprenticeCommitments.Web.UnitTests/Features/HowYourApprenticeshipWillBeDeliveredSteps.cs
@@ -1,21 +1,46 @@
 ﻿using SFA.DAS.ApprenticeCommitments.Web.AcceptanceTests;
 using SFA.DAS.ApprenticeCommitments.Web.AcceptanceTests.Features;
 using SFA.DAS.ApprenticeCommitments.Web.Pages.IdentityHashing;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Net.Http.Headers;
 using TechTalk.SpecFlow;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
 
 namespace SAF.DAS.ApprenticeCommitments.Web.UnitTests.Features
 {
     [Binding]
-    [Scope(Feature = "ConfirmYourEmployer")]
+    [Scope(Feature = "HowYourApprenticeshipWillBeDelivered")]
     public class HowYourApprenticeshipWillBeDeliveredSteps : StepsBase
     {
         private readonly TestContext _context;
         private readonly RegisteredUserContext _userContext;
         private HashedId _apprenticeshipId;
-        private string _employerName;
-        private bool? _employerNameConfirmed;
+
+        public HowYourApprenticeshipWillBeDeliveredSteps(TestContext context, RegisteredUserContext userContext) : base(context)
+        {
+            _context = context;
+            _userContext = userContext;
+            _apprenticeshipId = HashedId.Create(1235, _context.Hashing);
+
+            _context.OuterApi.MockServer.Given(
+                     Request.Create()
+                         .UsingAnyMethod()
+                         .WithPath("/apprentices/*/apprenticeships/*/howapprenticeshipdeliveredconfirmation"))
+                    .RespondWith(Response.Create()
+                        .WithStatusCode(200));
+        }
+
+        [Given("the apprentice has logged in")]
+        public void GivenTheApprenticeHasLoggedIn()
+        {
+            TestAuthenticationHandler.AddUser(_userContext.RegistrationId);
+            _context.Web.Client.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue(_userContext.RegistrationId.ToString());
+        }
+
+        [Given(@"the apprentice has not verified they have read the page")]
+        public void GivenTheApprenticeHasNotVerifiedTheyHaveReadThePage()
+        {
+        }
     }
 }
