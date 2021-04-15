7f5389eddbc938455a54f47d974607b0c142175c
mld3 <57175069+MarieDale3@users.noreply.github.com>
2021-04-15 11:14:07 +0100

    CS-392 test error for confirm page changes

--- src/SFA.DAS.ApprenticeCommitments.Web/Pages/Apprenticeships/Confirm.cshtml.cs
+++ src/SFA.DAS.ApprenticeCommitments.Web/Pages/Apprenticeships/Confirm.cshtml.cs
@@ -22,6 +22,7 @@ namespace SFA.DAS.ApprenticeCommitments.Web.Pages.Apprenticeships
         public bool? TrainingProviderConfirmation { get; set; } = null;
         public bool? ApprenticeshipDetailsConfirmation { get; set; } = null;
         public bool? RolesAndResponsibilitiesConfirmation { get; set; } = null;
+        public bool? HowApprenticeshipWillBeDeliveredConfirmation { get; set; } = null;
 
         public ConfirmApprenticeshipModel(AuthenticatedUserClient client)
         {
@@ -38,6 +39,7 @@ namespace SFA.DAS.ApprenticeCommitments.Web.Pages.Apprenticeships
             TrainingProviderConfirmation = apprenticeship.TrainingProviderCorrect;
             ApprenticeshipDetailsConfirmation = apprenticeship.ApprenticeshipDetailsCorrect;
             RolesAndResponsibilitiesConfirmation = apprenticeship.RolesAndResponsibilitiesCorrect;
+            HowApprenticeshipWillBeDeliveredConfirmation = apprenticeship.HowApprenticeshipDeliveredCorrect;
         }
     }
 }
