b9b753eb3947f600e0229c4b6141519b1f628288
chazzysb <cballagan@yahoo.com>
2021-05-12 12:12:56 +0100

    CS-542-CookiesBanner Initial Checkin

---
+++ src/SFA.DAS.ApprenticeCommitments.Web/Pages/Cookies/CookieBannerViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.ApprenticeCommitments.Web.Pages.Cookies
+{
+    public class CookieBannerViewModel
+    {
+        public string CookieConsentUrl => "/cookiesettings";
+
+        public string CookieDetailsUrl => "/cookiedetails";
+    }
+}
