017c2346d0eb6969e8fe654ae4790252f7532985
Ben Arnold <ben@arnoldfamily.me.uk>
2021-05-20 15:57:40 +0100

    Add Zendesk files to shared library

---
+++ src/SFA.DAS.Apprentice.SharedUi/ViewDataKeys.cs
@@ -0,0 +1,14 @@
+﻿namespace SFA.DAS.Apprentice.SharedUi
+{
+    public static class ViewDataKeys
+    {
+        public const string SelectedNavigationSection = "SharedUI.SelectedNavigationSection";
+        public const string HideNavigationLinks = "SharedUI.HiddenNavigationLinks";
+        public const string HideAccountHeader = "SharedUI.HiddenAccountHeader";
+        public const string ShowBetaPhaseBanner = "SharedUI.ShowBetaPhaseBanner";
+        public const string SuppressNavigationSection = "SharedUI.SuppressedNavigationSections";
+        public const string EnableGoogleAnalytics = "SharedUI.EnableGoogleAnalytics";
+        public const string ZenDeskConfiguration = "SharedUI.ZenDeskConfiguration";
+        public const string EnableCookieBanner = "SharedUI.EnableCookieBanner";
+    }
+}
---
+++ src/SFA.DAS.Apprentice.SharedUi/Zendesk/ZenDeskConfiguration.cs
@@ -0,0 +1,11 @@
+﻿#nullable disable
+
+namespace SFA.DAS.Apprentice.SharedUi.Zendesk
+{
+    public class ZenDeskConfiguration
+    {
+        public string SectionId { get; set; }
+        public string SnippetKey { get; set; }
+        public string CobrowsingSnippetKey { get; set; }
+    }
+}
