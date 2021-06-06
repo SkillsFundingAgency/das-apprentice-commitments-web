99ce60742348d6232ae9386d22eff9e48e30c0c1
Ben Arnold <ben@arnoldfamily.me.uk>
2021-06-06 06:17:41 +0100

    🔥 Remove unnecessary code

--- src/SFA.DAS.ApprenticeCommitments.Web/Pages/Apprenticeships/ConfirmYourApprenticeshipDetails.cshtml.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
-namespace SFA.DAS.ApprenticeCommitments.Web.Pages.Apprenticeships
-{
-    public class ConfirmYourApprenticeshipDetailsModel : PageModel
-    {
-    }
-}
