4624404ddfb8cd5aa02c7e03b742e2c81545e7b1
Paul Graham <paul.graham@coprime.co.uk>
2021-06-09 16:48:46 +0100

    removed using statement

--- src/SFA.DAS.ApprenticeCommitments.Web/Startup/ApplicationStartup.cs
+++ src/SFA.DAS.ApprenticeCommitments.Web/Startup/ApplicationStartup.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace SFA.DAS.ApprenticeCommitments.Web.Startup
 {
