32dc42491d0132a267392ce60449ff58d49f4043
Ben Arnold <ben@arnoldfamily.me.uk>
2021-07-07 14:41:18 +0100

    💥 Rename "statements" to "revisions" in API routes

--- src/SFA.DAS.ApprenticeCommitments.Web/Services/OuterApi/IOuterApiClient.cs
+++ src/SFA.DAS.ApprenticeCommitments.Web/Services/OuterApi/IOuterApiClient.cs
@@ -22,37 +22,37 @@ namespace SFA.DAS.ApprenticeCommitments.Web.Services.OuterApi
         [Get("/apprentices/{apprenticeid}/apprenticeships/{apprenticeshipid}")]
         Task<Apprenticeship> GetApprenticeship([Path] Guid apprenticeid, [Path] long apprenticeshipid);
 
-        [Post("/apprentices/{apprenticeid}/apprenticeships/{apprenticeshipid}/statements/{commitmentStatementId}/trainingproviderconfirmation")]
+        [Post("/apprentices/{apprenticeid}/apprenticeships/{apprenticeshipid}/revisions/{commitmentStatementId}/trainingproviderconfirmation")]
         Task ConfirmTrainingProvider(
             [Path] Guid apprenticeid, [Path] long apprenticeshipid, [Path] long commitmentStatementId,
             [Body] TrainingProviderConfirmationRequest confirmation);
 
-        [Post("/apprentices/{apprenticeid}/apprenticeships/{apprenticeshipid}/statements/{commitmentStatementId}/employerconfirmation")]
+        [Post("/apprentices/{apprenticeid}/apprenticeships/{apprenticeshipid}/revisions/{commitmentStatementId}/employerconfirmation")]
         Task ConfirmEmployer(
             [Path] Guid apprenticeid, [Path] long apprenticeshipid, [Path] long commitmentStatementId,
             [Body] EmployerConfirmationRequest confirmation);
 
-        [Post("/apprentices/{apprenticeid}/apprenticeships/{apprenticeshipid}/statements/{commitmentStatementId}/rolesandresponsibilitiesconfirmation")]
+        [Post("/apprentices/{apprenticeid}/apprenticeships/{apprenticeshipid}/revisions/{commitmentStatementId}/rolesandresponsibilitiesconfirmation")]
         Task ConfirmRolesAndResponsibilities(
             [Path] Guid apprenticeid, [Path] long apprenticeshipid, [Path] long commitmentStatementId,
             [Body] RolesAndResponsibilitiesConfirmationRequest confirmation);
 
-        [Post("/apprentices/{apprenticeid}/apprenticeships/{apprenticeshipid}/statements/{commitmentStatementId}/apprenticeshipdetailsconfirmation")]
+        [Post("/apprentices/{apprenticeid}/apprenticeships/{apprenticeshipid}/revisions/{commitmentStatementId}/apprenticeshipdetailsconfirmation")]
         Task ConfirmApprenticeshipDetails(
             [Path] Guid apprenticeid, [Path] long apprenticeshipid, [Path] long commitmentStatementId,
             [Body] ApprenticeshipDetailsConfirmationRequest confirmation);
 
-        [Post("/apprentices/{apprenticeid}/apprenticeships/{apprenticeshipid}/statements/{commitmentStatementId}/howapprenticeshipwillbedeliveredconfirmation")]
+        [Post("/apprentices/{apprenticeid}/apprenticeships/{apprenticeshipid}/revisions/{commitmentStatementId}/howapprenticeshipwillbedeliveredconfirmation")]
         Task ConfirmHowApprenticeshipDelivered(
             [Path] Guid apprenticeid, [Path] long apprenticeshipid, [Path] long commitmentStatementId,
             [Body] HowApprenticeshipDeliveredConfirmationRequest confirmation);
 
-        [Post("/apprentices/{apprenticeid}/apprenticeships/{apprenticeshipid}/statements/{commitmentStatementId}/apprenticeshipconfirmation")]
+        [Post("/apprentices/{apprenticeid}/apprenticeships/{apprenticeshipid}/revisions/{commitmentStatementId}/apprenticeshipconfirmation")]
         Task ConfirmApprenticeship(
             [Path] Guid apprenticeid, [Path] long apprenticeshipid, [Path] long commitmentStatementId,
             [Body] ApprenticeshipConfirmationRequest confirmation);
 
-        [Patch("/apprentices/{apprenticeId}/apprenticeships/{apprenticeshipId}/statements/{commitmentStatementId}")]
+        [Patch("/apprentices/{apprenticeId}/apprenticeships/{apprenticeshipId}/revisions/{commitmentStatementId}")]
         Task UpdateApprenticeship(
             [Path] Guid apprenticeId, [Path] long apprenticeshipId, [Path] long commitmentStatementId,
             [Body] JsonPatchDocument<Apprenticeship> patch);
