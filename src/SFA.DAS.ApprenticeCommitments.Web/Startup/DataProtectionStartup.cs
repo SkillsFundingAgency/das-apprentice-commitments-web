781e40079063e87703b03a0483250815e3d8f0bd
chazzysb <cballagan@yahoo.com>
2021-09-29 09:16:40 +0100

    CS-1096-CookiesSignInRedis-Web revert test update

--- src/SFA.DAS.ApprenticeCommitments.Web/Startup/DataProtectionStartup.cs
+++ src/SFA.DAS.ApprenticeCommitments.Web/Startup/DataProtectionStartup.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using StackExchange.Redis;
-using Azure.Extensions.AspNetCore.DataProtection.Blobs;
 
 namespace SFA.DAS.ApprenticeCommitments.Web.Startup
 {
@@ -14,27 +13,18 @@ namespace SFA.DAS.ApprenticeCommitments.Web.Startup
             DataProtectionConnectionStrings configuration,
             IWebHostEnvironment environment)
         {
-            //if (!environment.IsDevelopment() && configuration != null)
-            //{
-            //    var redisConnectionString = configuration.RedisConnectionString;
-            //    var dataProtectionKeysDatabase = configuration.DataProtectionKeysDatabase;
+            if (!environment.IsDevelopment() && configuration != null)
+            {
+                var redisConnectionString = configuration.RedisConnectionString;
+                var dataProtectionKeysDatabase = configuration.DataProtectionKeysDatabase;
 
-            //    var redis = ConnectionMultiplexer
-            //        .Connect($"{redisConnectionString},{dataProtectionKeysDatabase}");
+                var redis = ConnectionMultiplexer
+                    .Connect($"{redisConnectionString},{dataProtectionKeysDatabase}");
 
-            //    services.AddDataProtection()
-            //        .SetApplicationName("apprentice-commitments")
-            //        .PersistKeysToStackExchangeRedis(redis, "DataProtection-Keys");
-            //}
-
-            // This is released for a test only
-            var conx = "DefaultEndpointsProtocol=https;AccountName=comtstore01;AccountKey=Ie4vbuvys1aplZjaKUiIjdCDfiXfP+a37ctdnQMiL1HBy97Vzd9PNmWdMagrR4cOPD5DeeSG0uZVuj1G4mKJcA==;EndpointSuffix=core.windows.net";
-            var container = "cont01";
-            var blob = "keyblob";
-
-            services.AddDataProtection()
-                .PersistKeysToAzureBlobStorage(conx, container, blob)
-                .SetApplicationName("apprentice-commitments");
+                services.AddDataProtection()
+                    .SetApplicationName("apprentice-commitments")
+                    .PersistKeysToStackExchangeRedis(redis, "DataProtection-Keys");
+            }
 
             return services;
         }
