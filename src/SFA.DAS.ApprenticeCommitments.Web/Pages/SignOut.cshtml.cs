bee7dd02faead8dee1bf4b41397d9d183054fa4b
chazzysb <cballagan@yahoo.com>
2021-09-30 17:41:41 +0100

    CS-1096-CookiesSignInRedis-Web update 3

--- src/SFA.DAS.ApprenticeCommitments.Web/Pages/SignOut.cshtml.cs
+++ src/SFA.DAS.ApprenticeCommitments.Web/Pages/SignOut.cshtml.cs
@@ -2,14 +2,27 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Linq;
 
 namespace SFA.DAS.ApprenticeCommitments.Web.Pages
 {
     public class SignOutModel : PageModel
     {
-        public IActionResult OnGet() =>
-            SignOut(
-                CookieAuthenticationDefaults.AuthenticationScheme,
-                OpenIdConnectDefaults.AuthenticationScheme);
+        public IActionResult OnGet()
+        {
+            var result = SignOut(
+                            CookieAuthenticationDefaults.AuthenticationScheme,
+                            OpenIdConnectDefaults.AuthenticationScheme);
+
+            // clearup cookies
+            if (HttpContext.Request.Cookies.Count > 0)
+            {
+                var cookies = HttpContext.Request.Cookies.Where(x => x.Key.Contains("AspNetCore") || x.Key.Contains("Apprenticeships")).ToList();
+
+                cookies.ForEach(x => Response.Cookies.Delete(x.Key));
+            }
+
+            return result;
+        }
     }
 }
