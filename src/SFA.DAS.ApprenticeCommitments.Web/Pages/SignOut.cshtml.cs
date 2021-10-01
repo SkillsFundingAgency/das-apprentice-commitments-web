7a795c77030c9ef18827a1e50383147fc70bdda7
chazzysb <cballagan@yahoo.com>
2021-10-01 09:26:44 +0100

    CS-1096-CookiesSignInRedis-Web update 7

--- src/SFA.DAS.ApprenticeCommitments.Web/Pages/SignOut.cshtml.cs
+++ src/SFA.DAS.ApprenticeCommitments.Web/Pages/SignOut.cshtml.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Linq;
-
+using Microsoft.AspNetCore.Authentication;
 using SFA.DAS.ApprenticeCommitments.Web.Services;
 
 namespace SFA.DAS.ApprenticeCommitments.Web.Pages
@@ -18,19 +18,14 @@ namespace SFA.DAS.ApprenticeCommitments.Web.Pages
         }
 
         public IActionResult OnGet()
-        {
-            // clearup cookies
-            if (HttpContext.Request.Cookies.Count > 0)
-            {
-                var cookies = HttpContext.Request.Cookies.Where(x => x.Key.Contains("Apprenticeships") || x.Key.Contains("AspNetCore")).ToList();
-
-                cookies.ForEach(x => Response.Cookies.Delete(x.Key));
-            }
-
+        {            
             var result = SignOut(
                             CookieAuthenticationDefaults.AuthenticationScheme,
                             OpenIdConnectDefaults.AuthenticationScheme);
 
+            HttpContext.SignOutAsync().Wait();
+
+            // clearup cookies
             if (HttpContext.Request.Cookies.Count > 0)
             {
                 var cookies = HttpContext.Request.Cookies.Where(x => x.Key.Contains("Apprenticeships") || x.Key.Contains("AspNetCore")).ToList();
