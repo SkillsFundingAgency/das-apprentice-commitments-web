1119133c673ff1811d609766896068bf616061aa
chazzysb <cballagan@yahoo.com>
2021-10-03 14:24:57 +0100

    CS-1096-CookiesSignInRedis-Web update 15

--- src/SFA.DAS.ApprenticeCommitments.Web/Pages/SignOut.cshtml.cs
+++ src/SFA.DAS.ApprenticeCommitments.Web/Pages/SignOut.cshtml.cs
@@ -8,6 +8,7 @@ using SFA.DAS.ApprenticeCommitments.Web.Services;
 using System.Web;
 using Microsoft.AspNetCore.Http;
 
+
 namespace SFA.DAS.ApprenticeCommitments.Web.Pages
 {
     public class SignOutModel : PageModel
@@ -20,13 +21,36 @@ namespace SFA.DAS.ApprenticeCommitments.Web.Pages
         }
 
         public IActionResult OnGet()
-        {            
+        {
+            foreach (var cookie in Request.Cookies.Keys)
+            {
+                Response.Cookies.Delete(cookie);
+            }
+
             var result = SignOut(
                             CookieAuthenticationDefaults.AuthenticationScheme,
                             OpenIdConnectDefaults.AuthenticationScheme);
 
-            HttpContext.SignOutAsync().Wait();
+            //HttpContext.SignOutAsync().Wait();
 
+            //// clearup cookies
+            //if (HttpContext.Request.Cookies.Count > 0)
+            //{
+            //    var cookies = HttpContext.Request.Cookies.Where(x => x.Key.Contains("Apprenticeships") || x.Key.Contains("AspNetCore")).ToList();
+
+            //    //cookies.ForEach(x => Response.Cookies.Delete(x.Key));
+            //    cookies.ForEach(x =>
+            //    {
+            //        var options = new CookieOptions { Expires = System.DateTime.Now.AddDays(-1) };
+            //        this.Response.Cookies.Append(x.Key, x.Value, options);
+            //    });
+            //}
+
+            return result;
+        }
+
+        public void OnPost()
+        {
             // clearup cookies
             if (HttpContext.Request.Cookies.Count > 0)
             {
@@ -39,8 +63,6 @@ namespace SFA.DAS.ApprenticeCommitments.Web.Pages
                     this.Response.Cookies.Append(x.Key, x.Value, options);
                 });
             }
-
-            return result;
         }
     }
 }
