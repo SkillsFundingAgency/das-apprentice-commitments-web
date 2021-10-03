524302c55f19f9222b2c3761d266fad72df8aee5
chazzysb <cballagan@yahoo.com>
2021-10-03 14:45:35 +0100

    CS-1096-CookiesSignInRedis-Web update 16

--- src/SFA.DAS.ApprenticeCommitments.Web/Startup/AuthenticationStartup.cs
+++ src/SFA.DAS.ApprenticeCommitments.Web/Startup/AuthenticationStartup.cs
@@ -38,18 +38,18 @@ namespace SFA.DAS.ApprenticeCommitments.Web.Startup
                 {
                     options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
                     options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
-                    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                    options.DefaultSignOutScheme = OpenIdConnectDefaults.AuthenticationScheme;
+                    //options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    //options.DefaultSignOutScheme = OpenIdConnectDefaults.AuthenticationScheme;
                 })
                 .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
                 {
                     options.Cookie.Name = ".Apprenticeships.Application";
                     //options.Cookie.HttpOnly = true;
-                    options.Cookie.SameSite = SameSiteMode.Lax;
-                    options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+                    //options.Cookie.SameSite = SameSiteMode.Lax;
+                    //options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
                     options.SlidingExpiration = true;
                     options.ExpireTimeSpan = System.TimeSpan.FromHours(1);
-                    options.CookieManager = new ChunkingCookieManager();
+                    //options.CookieManager = new ChunkingCookieManager();
                 })
                 .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
                 {
