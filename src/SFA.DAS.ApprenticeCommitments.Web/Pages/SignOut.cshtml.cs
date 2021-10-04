1bc5c9d15e6c5ab88984062c877dad1bf86f893a
chazzysb <cballagan@yahoo.com>
2021-10-04 13:10:39 +0100

    CS-1096-CookiesSignInRedis-Web sonar cloud

--- src/SFA.DAS.ApprenticeCommitments.Web/Pages/SignOut.cshtml.cs
+++ src/SFA.DAS.ApprenticeCommitments.Web/Pages/SignOut.cshtml.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using SFA.DAS.ApprenticeCommitments.Web.Services;
+
 namespace SFA.DAS.ApprenticeCommitments.Web.Pages
 {
     public class SignOutModel : PageModel
