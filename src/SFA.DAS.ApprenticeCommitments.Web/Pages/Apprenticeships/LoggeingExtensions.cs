356a1c7d029b6d0cc1d61dd1c3c0a90dffe71168
Ben Arnold <ben@arnoldfamily.me.uk>
2021-11-16 15:31:31 +0000

    🐛 Register when landing on /apprenticeships with a registration code cookie

---
+++ src/SFA.DAS.ApprenticeCommitments.Web/Pages/Apprenticeships/LoggeingExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+
+namespace SFA.DAS.ApprenticeCommitments.Web.Pages.Apprenticeships
+{
+    public static class LoggeingExtensions
+    {
+        public static IDisposable BeginPropertyScope(
+            this ILogger logger,
+            params ValueTuple<string, object>[] properties)
+        {
+            var dictionary = properties.ToDictionary(p => p.Item1, p => p.Item2);
+            return logger.BeginScope(dictionary);
+        }
+
+    }
+}
