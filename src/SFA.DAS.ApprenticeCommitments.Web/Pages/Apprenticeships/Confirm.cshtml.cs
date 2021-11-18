17fd6b6dc692dcbf44c3c6c45edbce19ce7a1ce4
Paul Graham <paul.graham@coprime.co.uk>
2021-11-18 16:32:39 +0000

    Fixed status message for ApprenticeshipDetails

--- src/SFA.DAS.ApprenticeCommitments.Web/Pages/Apprenticeships/Confirm.cshtml.cs
+++ src/SFA.DAS.ApprenticeCommitments.Web/Pages/Apprenticeships/Confirm.cshtml.cs
@@ -112,7 +112,7 @@ namespace SFA.DAS.ApprenticeCommitments.Web.Pages.Apprenticeships
             RevisionId = apprenticeship.RevisionId;
             EmployerConfirmation = apprenticeship.EmployerCorrect;
             TrainingProviderConfirmation = apprenticeship.TrainingProviderCorrect;
-            ApprenticeshipDetailsConfirmation = apprenticeship.ApprenticeshipDetailsCorrect;
+            ApprenticeshipDetailsConfirmation = apprenticeship.ApprenticeshipDetailsCorrect == true ? true : (bool?)null;
             RolesAndResponsibilitiesConfirmation = apprenticeship.RolesAndResponsibilitiesConfirmations.IsConfirmed() ? true : (bool?)null;
             HowApprenticeshipWillBeDeliveredConfirmation = apprenticeship.HowApprenticeshipDeliveredCorrect;
             ChangeNotifications = apprenticeship.ChangeOfCircumstanceNotifications;
