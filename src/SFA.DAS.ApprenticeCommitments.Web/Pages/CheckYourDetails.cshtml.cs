44e497d1df0b294a9d16d4e623e6c89093ddc487
Ben Arnold <ben@arnoldfamily.me.uk>
2022-02-24 11:41:34 +0000

    🔥 Remove update apprentice endpoint

--- src/SFA.DAS.ApprenticeCommitments.Web/Pages/CheckYourDetails.cshtml.cs
+++ src/SFA.DAS.ApprenticeCommitments.Web/Pages/CheckYourDetails.cshtml.cs
@@ -4,18 +4,22 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using SFA.DAS.ApprenticeCommitments.Web.Services;
 using SFA.DAS.ApprenticePortal.Authentication;
+using SFA.DAS.ApprenticePortal.SharedUi.Filters;
 using SFA.DAS.ApprenticePortal.SharedUi.Menu;
 
 namespace SFA.DAS.ApprenticeCommitments.Web.Pages
 {
     [HideNavigationBar]
+    [RequiresIdentityConfirmed]
     public class CheckYourDetails : PageModel
     {
         private readonly ApprenticeApi _apprentices;
+        private readonly NavigationUrlHelper _urlHelper;
 
-        public CheckYourDetails(ApprenticeApi _apprentices)
+        public CheckYourDetails(ApprenticeApi _apprentices, NavigationUrlHelper urlHelper)
         {
             this._apprentices = _apprentices;
+            _urlHelper = urlHelper;
         }
 
         public string FirstName { get; private set; } = null!;
@@ -26,7 +30,7 @@ namespace SFA.DAS.ApprenticeCommitments.Web.Pages
             [FromServices] AuthenticatedUser user)
         {
             var apprentice = await _apprentices.TryGetApprentice(user.ApprenticeId);
-            if (apprentice == null) return RedirectToPage("Account");
+            if (apprentice == null) return Redirect(_urlHelper.Generate(NavigationSection.ApprenticeAccounts, $"Account"));
 
             FirstName = apprentice.FirstName;
             LastName = apprentice.LastName;
--- src/SFA.DAS.ApprenticeCommitments.Web/Services/ApprenticeApi.cs
+++ src/SFA.DAS.ApprenticeCommitments.Web/Services/ApprenticeApi.cs
@@ -1,8 +1,4 @@
-﻿using Microsoft.AspNetCore.JsonPatch;
-using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
-using RestEase;
-using SFA.DAS.ApprenticeCommitments.Web.Exceptions;
+﻿using RestEase;
 using SFA.DAS.ApprenticeCommitments.Web.Services.OuterApi;
 using System;
 using System.Threading.Tasks;
@@ -46,35 +42,6 @@ namespace SFA.DAS.ApprenticeCommitments.Web.Services
             => _client.RegistrationFirstSeenOn(registrationCode,
                    new RegistrationFirstSeenOnRequest { SeenOn = seenOn });
 
-        internal async Task CreateApprentice(Apprentice apprentice)
-        {
-            await TryApi(() => _client.CreateApprenticeAccount(apprentice));
-        }
-
-        internal async Task UpdateApprentice(Guid apprenticeId, string firstName, string lastName, DateTime? dateOfBirth = null)
-        {
-            await TryApi(async () =>
-            {
-                var patch = new JsonPatchDocument<Apprentice>()
-                    .Replace(x => x.FirstName, firstName)
-                    .Replace(x => x.LastName, lastName);
-
-                if (dateOfBirth != null)
-                    patch = patch.Replace(x => x.DateOfBirth, dateOfBirth);
-
-                await _client.UpdateApprentice(apprenticeId, patch);
-            });
-        }
-
-        internal async Task AcceptTermsOfUse(Guid apprenticeId)
-        {
-            await TryApi(async () =>
-            {
-                await _client.UpdateApprentice(apprenticeId,
-                    new JsonPatchDocument<Apprentice>().Replace(x => x.TermsOfUseAccepted, true));
-            });
-        }
-
         internal async Task MatchApprenticeToApprenticeship(string registrationId, Guid apprenticeId)
         {
             await _client.ClaimApprenticeship(new ApprenticeshipAssociation
@@ -82,20 +49,6 @@ namespace SFA.DAS.ApprenticeCommitments.Web.Services
                 RegistrationId = registrationId,
                 ApprenticeId = apprenticeId,
             });
-        }
-
-        private async Task TryApi(Func<Task> action)
-        {
-            try
-            {
-                await action();
-            }
-            catch (ApiException ex) when (ex.StatusCode == System.Net.HttpStatusCode.BadRequest)
-            {
-                var value = ex.Content!;
-                var errors = JsonConvert.DeserializeObject<ValidationProblemDetails>(value);
-                throw new DomainValidationException(errors);
-            }
         }
     }
 }
