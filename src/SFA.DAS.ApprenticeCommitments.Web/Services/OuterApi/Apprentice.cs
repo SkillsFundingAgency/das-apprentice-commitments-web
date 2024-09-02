772034580a374be3216da8e94abccb744da2874b
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2024-09-02 22:57:10 +0100

    FAI-1314 - fix issues with gov login

--- src/SFA.DAS.ApprenticeCommitments.Web/Services/OuterApi/Apprentice.cs
+++ src/SFA.DAS.ApprenticeCommitments.Web/Services/OuterApi/Apprentice.cs
@@ -9,7 +9,7 @@ namespace SFA.DAS.ApprenticeCommitments.Web.Services.OuterApi
         public string FirstName { get; set; } = null!;
         public string LastName { get; set; } = null!;
         public string Email { get; set; } = null!;
-        public DateTime DateOfBirth { get; set; }
+        public DateTime? DateOfBirth { get; set; }
         public bool TermsOfUseAccepted { get; set; }
     }
 }
